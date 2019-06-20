0f6ea17ac29e96236db2fe79ae05a9fe4b8bc9f5
Junaid <jahmad095@gmail.com>
2019-06-20 18:24:22 -0400

    added post requests as well as sent data to database with each post in new form as well as with each submission

--- routes/polls.js
+++ routes/polls.js
@@ -3,7 +3,47 @@
 const express = require('express');
 const router  = express.Router();
 
+
+
 module.exports = (knex) => {
+
+  //new form submit
+  router.post("/", (req, res) => {
+
+    if (req.body) {
+
+      let templateVars = {
+        question: req.body.question,
+        description: req.body.description,
+        options: req.body.options,
+        email: req.body.email,
+        err: ""
+      };
+
+      let valid = templateVars.question && templateVars.description && templateVars.options.length > 1 && templateVars.email
+
+      if (valid) {
+
+        knex("polls")
+        .insert({'question': templateVars.question, 'description': templateVars.description, 'options': templateVars.options, 'email': templateVars.email })
+        .then(() => console.log("it worked"))
+        .catch((err) => {console.log(err); throw err})
+        .finally(() => knex.destroy());
+
+        res.render("results", templateVars).status(200);
+
+      } else {
+
+        templateVars.err = "Missing information, please validate."
+        res.render("new_poll", templateVars).status(400);
+      }
+    } else {
+      let templateVars = {
+        err: "No information entered into form, Please fill in all fields"
+      }
+      res.render("new_poll", templateVars).status(400);
+    }
+  });
 
   router.get("/new", (req, res) => {
     res.render("new_poll");
