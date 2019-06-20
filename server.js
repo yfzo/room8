e412a885d0c87ae6a671d78fece4dde72b3b73b3
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2019-06-20 15:31:38 -0400

    finish basic homepage markup

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const knexLogger  = require('knex-logger');
 
 // Seperated Routes for each Resource
 const pollsRoutes = require("./routes/polls");
+const submissionRoutes = require("./routes/submissions");
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -36,12 +37,29 @@ app.use("/styles", sass({
 app.use(express.static("public"));
 
 // Mount all resource routes
-app.use("/api/polls", pollsRoutes(knex));
+app.use("/polls", pollsRoutes(knex));
+app.use("/submissions", submissionRoutes(knex));
 
 // Home page
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+
+//show poll for voter page
+app.get("/submissions/:id", (req, res) => {
+
+  let templateVars = {
+    //poll: submissions[req.params.id], // pass poll based on ID
+    err: "submission link is invalid, provide valid link, or contact poll admin"
+  };
+
+  if (req.params.id === submissions.sub_Id){
+    res.render("submission", templateVars);
+  } else {
+    res.render("index", templateVars); //prompt top bander for illegal entry
+  }
+})
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
