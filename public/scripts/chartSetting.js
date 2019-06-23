d3515fd6108a3dc7443dae5080939a04209a79ea
Junaid <jahmad095@gmail.com>
2019-06-23 18:17:00 -0400

    finally result is the right results

--- public/scripts/chartSetting.js
+++ public/scripts/chartSetting.js
@@ -10,13 +10,21 @@ $(function() {
           labels: response.options,
           datasets: [
             {
-              label: "Scores",
+              label: response.options,
               backgroundColor: generateRandomColoursArray(response.options.length),
               data: response.scores
             }
           ]
         },
         options: {
+          elements: {
+            center: {
+            text: topScore(response.options, response.scores),
+            color: '#36A2EB', //Default black
+            fontStyle: 'Helvetica', //Default Arial
+            sidePadding: 15 //Default 20 (as a percentage)
+          }
+        }
         },
       });
     },
@@ -33,4 +41,55 @@ $(function() {
     return colours;
   }
 
+  function topScore(options, score) {
+    console.log("SCORE " , score);
+    let topIndex = score.indexOf(Math.max(...score));
+    console.log("TOP INDEX ", topIndex);
+    return options[topIndex];
+  }
+
 });
+
+Chart.pluginService.register({
+  beforeDraw: function (chart) {
+    if (chart.config.options.elements.center) {
+      //Get ctx from string
+      var ctx = chart.chart.ctx;
+
+      //Get options from the center object in options
+      var centerConfig = chart.config.options.elements.center;
+      var fontStyle = centerConfig.fontStyle || 'Arial';
+      var txt = centerConfig.text;
+      var color = centerConfig.color || '#000';
+      var sidePadding = centerConfig.sidePadding || 20;
+      var sidePaddingCalculated = (sidePadding/100) * (chart.innerRadius * 2)
+      //Start with a base font of 30px
+      ctx.font = "30px " + fontStyle;
+
+      //Get the width of the string and also the width of the element minus 10 to give it 5px side padding
+      var stringWidth = ctx.measureText(txt).width;
+      var elementWidth = (chart.innerRadius * 2) - sidePaddingCalculated;
+
+      // Find out how much the font can grow in width.
+      var widthRatio = elementWidth / stringWidth;
+      var newFontSize = Math.floor(30 * widthRatio);
+      var elementHeight = (chart.innerRadius * 2);
+
+      // Pick a new font size so it will not be larger than the height of label.
+      var fontSizeToUse = Math.min(newFontSize, elementHeight);
+
+      //Set font settings to draw it correctly.
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'middle';
+      var centerX = ((chart.chartArea.left + chart.chartArea.right) / 2);
+      var centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 2);
+      ctx.font = fontSizeToUse+"px " + fontStyle;
+      ctx.fillStyle = color;
+
+      //Draw text in center
+      ctx.fillText(txt, centerX, centerY);
+    }
+  }
+});
+
+
